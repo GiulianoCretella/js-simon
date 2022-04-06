2e0acfd566301be1db40a7b4ec12ebfeabb09cb6
GiulianoCretella <giulianocretella.1@gmail.com>
2022-04-06 15:17:16 +0200

    array pc creato

--- js/script.js
+++ js/script.js
@@ -5,9 +5,42 @@
 
 // creo funzione per numeri random
 // creo un Array
-// con un ciclo while 
+// creo un ciclo while 
 // inserisco al suo interno 5 numeri random controllando che non ci siano ripetizioni
 // creo un timer di trenta secondi 
-// tramite prompt faccio inserire i numeri che ricorda l'utente
+// tramite funzione e prompt faccio inserire i numeri che ricorda l'utente
 // ottenuti i 5 numeri li confronto con quelli dell'array
 //  stampo i numeri che trovo uguali
+
+// creo funzione per numeri random
+function getRandomInt(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min)
+}
+// creo un Array
+// creo un ciclo while
+// inserisco al suo interno 5 numeri random controllando che non ci siano ripetizioni
+numberArray=[];
+for( let i = 1; numberArray.length < 5;i++){
+    let pcNumber = getRandomInt(1, 50)
+    if(!numberArray.includes(pcNumber)){
+        numberArray.push(pcNumber)
+    }
+}
+ console.log(numberArray);
+
+let div = document.getElementById('app');
+div.append('Memorizza questi numeri!:' + ' '+ numberArray.join(' - '));
+
+// creo un timer di trenta secondi 
+// tramite funzione e prompt faccio inserire i numeri che ricorda l'utente
+
+numeriUtente=[];
+setTimeout(numeriMemorizzati, 3000);
+function numeriMemorizzati(){
+    numeriUser = prompt('ricordi i numeri?');
+    numeriUtente.push(numeriUser)
+    return numeriUtente
+}
+console.log(numeriUtente)
