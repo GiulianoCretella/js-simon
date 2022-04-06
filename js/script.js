7b1fa77d6309701b697fa31ba60c3b08f7c5ff0a
GiulianoCretella <giulianocretella.1@gmail.com>
2022-04-06 13:21:51 +0200

    punti da eseguire

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Visualizzare in pagina 5 numeri casuali (in un range tra 1 e 100). Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+// creo funzione per numeri random
+// creo un Array
+// con un ciclo while 
+// inserisco al suo interno 5 numeri random controllando che non ci siano ripetizioni
+// creo un timer di trenta secondi 
+// tramite prompt faccio inserire i numeri che ricorda l'utente
+// ottenuti i 5 numeri li confronto con quelli dell'array
+//  stampo i numeri che trovo uguali
